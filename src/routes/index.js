93abde5d345c31bd6639cc19fcc22ab6bad5c192
Vijayrajvijay <vr104555@gmail.com>
2023-12-10 16:33:38 +0530

    created success

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,9 @@ import express from 'express'
 import UserRouter from './user.js'
 
 const router = express.Router()
-
+router.get("/", (req, res) => {
+    res.status(200).send(` <h1> Welcome to our Password Reset backend</h1>`);
+  });
 router.use('/user',UserRouter)
 
 export default router
